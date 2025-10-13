32cbb25899fad7b8fbeef8e29a501725332adfdc
cosminsaracil <cosmin.saracil1@gmail.com>
2025-10-13 19:31:10 +0300

    changed check connection method

--- node-be/clients/index.js
+++ node-be/clients/index.js
@@ -5,9 +5,11 @@ export const checkConnections = async (req, res) => {
   let mongoStatus = "stopped";
   let redisStatus = "stopped";
 
-  // Test MongoDB
+  // MongoDB
   try {
-    const client = new MongoClient("mongodb://localhost:27017");
+    const client = new MongoClient("mongodb://localhost:27017", {
+      serverSelectionTimeoutMS: 1000,
+    });
     await client.connect();
     await client.db("admin").command({ ping: 1 });
     mongoStatus = "running";
@@ -16,15 +18,25 @@ export const checkConnections = async (req, res) => {
     mongoStatus = "stopped";
   }
 
-  // Test Redis
+  // Redis
   try {
-    const redis = new Redis(6379, "localhost");
-    await redis.ping();
-    redisStatus = "running";
+    const redis = new Redis({
+      port: 6379,
+      host: "localhost",
+      connectTimeout: 1000,
+      retryStrategy: null, // prevent retries
+    });
+    const pong = await redis.ping();
+    if (pong === "PONG") {
+      redisStatus = "running";
+    }
     redis.disconnect();
   } catch (e) {
     redisStatus = "stopped";
   }
+
+  console.log(`MongoDB: ${mongoStatus}`);
+  console.log(`Redis: ${redisStatus}`);
 
   res.json({ mongo: mongoStatus, redis: redisStatus });
 };
